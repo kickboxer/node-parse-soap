58e50585b3e8af616fc77842a68448ad9b047bb3
Maxim <m.mnatsakanov@aisnovations.com>
2015-09-18 16:54:08 +0300

    #28131 small fixes

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-var express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
+var mongoose = require('mongoose'),
     soap = require('soap'),
     _ = require('lodash'),
+    config = require('./settings'),
     url = 'http://www.nbrb.by/Services/ExRates.asmx?WSDL',
     date = new Date,
     args = { onDate: date.toISOString() };
 
-app.set('port', (process.env.PORT || 6000));
+var http = require("http");
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-});
+var server = http.createServer();
+server.listen(8888);
 
-mongoose.connect('mongodb://127.0.0.1:3001/meteor');
+mongoose.connect(config.mongoUrl);
 var db = mongoose.connection;
 db.on('error', console.error);
 db.once('open', function() {
 
     var ratesSchema = new mongoose.Schema({
         attributes: { type: Object },
-        Cur_QuotName: String,
-        Cur_Scale: Number,
-        Cur_OfficialRate: Number,
-        Cur_Code: Number,
-        Cur_Abbreviation: String
+        name: String,
+        scale: Number,
+        rate: Number,
+        code: Number,
+        abbreviation: String
     });
     mongoose.connection.db.dropCollection('exrates', function(err, result) {
         console.log(result);
@@ -40,11 +38,11 @@ db.once('open', function() {
                 _.forEach(rates, function(r) {
                     var rate = new ExRates({
                         attributes: [Object],
-                        Cur_QuotName: r.Cur_QuotName,
-                        Cur_Scale: r.Cur_Scale,
-                        Cur_OfficialRate: r.Cur_OfficialRate,
-                        Cur_Code: r.Cur_Code,
-                        Cur_Abbreviation: r.Cur_Abbreviation
+                        name: r.Cur_QuotName,
+                        scale: r.Cur_Scale,
+                        rate: r.Cur_OfficialRate,
+                        code: r.Cur_Code,
+                        abbreviation: r.Cur_Abbreviation
                     });
 
                     rate.save(function(err, rate) {
